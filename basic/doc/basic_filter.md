00100805922ca722db9147b097173b99f96f24bb
Ahn <abu89@naver.com>
2022-07-10 22:56:03 +0900

    [SECURITY] update basic filter in authentication
    + Authentication
      + AnonymousAuthenticationFilter
      + SessionManagementFilter & ConcurrentSessionFilter
    + Authorization
      + URL based authorization
      + expression

--- basic/doc/basic_filter.md
+++ basic/doc/basic_filter.md
@@ -72,3 +72,83 @@ http.rememberMe()
   - 디폴트는 false
 - userDetailsService(userDetailsService)
   - 리멤버미 기능이 수행될 때, 시스템에 있는 사용자 계정을 조회하는데 필요한 클래스
+
+### AnonymousAuthenticationFilter
+
+<p align="center"><img src="./img/basic_filter_4.png" width="80%"></p>
+
+익명 사용자 인증 처리를 위한 필터이며, 익명 사용자와 인증 사용자를 구분해 처리하기 위한 목적으로 사용된다.  
+굳이 익명 토큰을 만드는 이유는 뭘까? 바로 `SecurityInterceptor`에서 최종적인 리소스 접근에 대한 확인을 할 때,
+토큰 자체가 존재하지 않으면 예외를 발생시키기 때문에 익명 토큰을 만들어 통과시켜준다.
+
+### 동시 세션 제어
+
+서버에서 `같은 계정에 대해` 최대 세션 허용 갯수가 초과되는 경우 어떤 전략으로 세션을 관리할까?
+
+1. 이전 사용자 세션 만료
+   - 사용자 1이 인증에 성공해서 세션이 생성됨
+   - 사용자 2가 접속하는 경우 사용자 1의 세션을 만료 시킴
+   - 사용자 1이 다시 접속하는 경우 세션이 만료되어 재발급 받아야함
+2. 현재 사용자 인증 실패
+   - 사용자 1이 인증에 성공해서 세션이 생성됨
+   - 사용자 2가 접속하지만, 세션을 발생하지 못하고 인증 예외 발생
+
+```text
+http
+    .sessionManagement()
+```
+
+- maximumSession(1)
+  - 최대 허용 세션 수
+  - -1 인 경우 무제한 로그인 세션
+- maxSessionPreventsLogin(true)
+  - 동시 로그인 차단을 위한 옵션(2번 전략)
+  - false(default) 이면, 기존 세션을 만료시킨다 (1번 전략)
+- invalidSessionUrl("/invalid")
+  - 세션이 유효하지 않을 때 이동하는 페이지
+- expiredUrl("/expired")
+  - 세션 만료 시 이동할 페이지
+- sessionFixation().changeSessionId()
+  - 세션 고정보호용 옵션(default)
+  - 사용자가 로그인할 때마다 새로운 세션을 바꿔준다
+- sessionCreationPolicy(If_Required)
+  - 세션 생성 정책
+  - Always: 항상 생성
+  - If_Required: 필요시 생성, 디폴트
+  - Never: 시큐리티가 생성하진 않지만, 이미 존재한다면 사용
+  - Stateless: 시큐리티가 생성하지 않고, 존재해도 사용안함
+
+### SessionManagementFilter & ConcurrentSessionFilter
+
+<p align="center"><img src="./img/basic_filter_5.png" width="80%"></p>
+
+- SessionManagementFilter
+  1. 세션관리
+  2. 동시 세션 제어
+  3. 세션 고정 보호
+  4. 세션 생성 정책
+- ConcurrentSessionFilter
+  - 매 요청마다 사용자의 세션 만료 여부를 체크하는 필터
+  - 세션 만료시 `session.isExpired() == true`
+    - 로그아웃 처리
+    - 오류 페이지 응답
+
+--- 
+
+## 인가 API
+
+### 권한 설정
+
+1. 선언적 방식
+   - URL
+     - `http.antMatchers("/users/**").hasRole("USER")`
+   - Annotation
+     - `@PreAuthorize("hasRole("USER")")`
+2. 동적 방식
+   - URL
+   - Method
+
+### 표현식
+
+<p align="center"><img src="./img/basic_filter_6.png" width="80%"></p>
+
--- basic/src/main/java/study/security/config/SecurityConfig.java
+++ basic/src/main/java/study/security/config/SecurityConfig.java
@@ -3,9 +3,19 @@ package study.security.config;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,14 +25,53 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain httpSecurityFilter(HttpSecurity http) throws Exception {
         log.info("Security Filter chain");
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated();
 
+        // 인증
         addUsernamePasswordAuthenticationFilter(http);
         addLogoutFilter(http);
+        addSessionManagementFilter(http);
+
+        // 인가
+        http
+                .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").hasRole("ADMIN")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
+                .anyRequest().authenticated();
 
         return http.build();
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("1111")
+                .roles("USER")
+                .build();
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("sys")
+                .password("1111")
+                .roles("SYS","USER")
+                .build();
+        UserDetails user3 = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("1111")
+                .roles("ADMIN","USER","SYS")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(user2);
+        users.createUser(user3);
+        return users;
     }
 
     private void addUsernamePasswordAuthenticationFilter(HttpSecurity http) throws Exception {
@@ -69,5 +118,12 @@ public class SecurityConfig {
                 .rememberMeParameter("remember")
                 .tokenValiditySeconds(3600)
                 .alwaysRemember(true);
+    }
+
+    private void addSessionManagementFilter(HttpSecurity http) throws Exception {
+        http
+                .sessionManagement()
+                .maximumSessions(1)
+                .maxSessionsPreventsLogin(true);
     }
 }
